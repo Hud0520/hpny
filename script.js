b3ece7eff2c34458c90207434c2666982854f1c3
hungd <tm.vn06082024@gmail.com>
2025-12-31 21:19:34 +0700

    t

--- script.js
+++ script.js
@@ -116,7 +116,7 @@ const store = {
 	state: {
 		// will be unpaused in init()
 		paused: true,
-		soundEnabled: false,
+		soundEnabled: true,
 		menuOpen: false,
 		openHelpTopic: null,
 		fullscreen: isFullscreen(),
@@ -845,6 +845,9 @@ function init() {
 
 	// Apply initial config
 	configDidUpdate();
+
+	// auto-enable sound
+	soundManager.resumeAll();
 }
 
 
